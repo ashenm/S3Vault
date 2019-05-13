acdda78bf41ec78ac106f00649b1890f62072853
Ashen Gunaratne <mail@ashenm.ml>
2019-05-13 10:56:02 +0000

    DocumentRoot subfolder dist

--- utils/server.py
+++ utils/server.py
@@ -1,20 +1,22 @@
 #!/usr/bin/env python3
 
 from sys import argv
-from os.path import isfile
+from os import getcwd
+from os.path import join, isfile
 from functools import partial
 from http.server import SimpleHTTPRequestHandler, test
 
 class VautlRequestHandler(SimpleHTTPRequestHandler):
-    
+
     def __init__(self, *args, index='vault.html', **kwargs):
-        self.index = index
+        self.root = join(getcwd(), 'dist')
+        self.index = join(self.root, index)
         super().__init__(*args, **kwargs)
 
     def translate_path(self, path):
         print(path)
-        if isfile(path.lstrip('/')):
-            return path.lstrip('/')
+        if isfile(join(self.root, path.lstrip('/'))):
+            return join(self.root, path.lstrip('/'))
         return self.index
 
 if __name__ == '__main__':
